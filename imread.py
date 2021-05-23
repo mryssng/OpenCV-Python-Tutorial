7f7a5eae6eb7ec98eda914273740508532601ff5
Jam Ytse <mryssng@gmail.com>
2021-05-23 15:49:03 +0900

    imread.pyを更新

--- imread.py
+++ imread.py
@@ -7,19 +7,25 @@
   * Load image : imread
   https://docs.opencv.org/master/d4/da8/group__imgcodecs.html#ga288b8b3da0892bd651fce07b3bbd3a56
 
-  * Image data : https://unsplash.com/photos/pEGyLHAPfVw
+  * Image data : https://unsplash.com/photos/VTvnoNBowZs
 '''
 
 import cv2
 
-# Load file without imread flag
+# Read file without imread flag
 image1 = cv2.imread('images/duck.jpg')
 cv2.imwrite('images/duck_out.jpg', image1)
 
-# Load file without imread flag
+# Read file without imread flag
 image2 = cv2.imread('images/duck.jpg', cv2.IMREAD_GRAYSCALE)
 cv2.imwrite('images/duck_gray.jpg', image2)
 
-# print()関数の引数endで開業の代わりに任意の文字列が挿入される
-print('Image Size : ', end='')
-print(src.shape)
+# When the image file can't be read, imread returns "None".
+image3 = cv2.imread('images/_duck.jpg')
+
+# Example to check if image is being read
+image4 = cv2.imread('images/duck.jpg')
+if image4 is not None:
+    print('Image file was read.')
+else:
+    print('Image file was NOT read.')
