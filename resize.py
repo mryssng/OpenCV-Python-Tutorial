8c6ba5629542c25ecc49e04fc3e205c340ae853a
Jam Ytse <mryssng@gmail.com>
2021-11-03 14:11:37 +0900

    Update resize.py

--- resize.py
+++ resize.py
@@ -8,11 +8,14 @@
     https://docs.opencv.org/4.5.2/da/d54/group__imgproc__transform.html#ga47a974309e9102f5f08231edc7e7529d
 
   * Image data : https://unsplash.com/photos/VTvnoNBowZs
+                 https://www.pexels.com/ja-jp/photo/439391/
 '''
 
 import cv2
+import numpy as np
+import matplotlib.pyplot as plt
 
-# Read file
+# # Read file
 image = cv2.imread('images/duck.jpg')
 
 # Resize #1
@@ -23,3 +26,41 @@ cv2.imwrite('images/resize_1.jpg', result)
 h, w = image.shape[:2]
 result = cv2.resize(image, (w * 2, h))
 cv2.imwrite('images/resize_2.jpg', result)
+
+# Comparing Interpolation Methods
+# # Read file
+image = cv2.imread('images/building.jpg')
+h, w = image.shape[:2]
+
+# INTER_NEAREST
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST)
+cv2.imwrite('images/resize_INTER_NEAREST.jpg', result)
+
+# INTER_LINEAR
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR)
+cv2.imwrite('images/resize_INTER_LINEAR.jpg', result)
+
+# INTER_CUBIC
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_CUBIC)
+cv2.imwrite('images/resize_INTER_CUBIC.jpg', result)
+
+# INTER_AREA
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+cv2.imwrite('images/resize_INTER_AREA.jpg', result)
+
+# INTER_LANCZOS4
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LANCZOS4)
+cv2.imwrite('images/resize_INTER_LANCZOS4.jpg', result)
+
+# INTER_LINEAR_EXACT
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_LINEAR_EXACT)
+cv2.imwrite('images/resize_INTER_LINEAR_EXACT.jpg', result)
+
+# INTER_NEAREST_EXACT
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_NEAREST_EXACT)
+cv2.imwrite('images/resize_INTER_NEAREST_EXACT.jpg', result)
+
+# INTER_MAX
+result = cv2.resize(image, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_MAX)
+cv2.imwrite('images/resize_INTER_MAX.jpg', result)
+
